e188819e304786522930332efc7e5ba18134e04b
Jessé Luis Silveira <jesse-silveira@hotmail.com>
2014-10-27 16:42:33 -0200

    27/10/2014 - Criar a classe para chamar avaliadores.

---
+++ EvaluationForm/src/br/com/evaluationform/SelecionarAvaliador.java
@@ -0,0 +1,35 @@
+package br.com.evaluationform;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class SelecionarAvaliador extends Activity{
+	
+	private ListView listSelectAv;
+	private Button btFinaliza;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.selecionar_avaliador);
+		
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+		}
+		this.inicializaComponentes();
+	
+		
+	}
+	private void inicializaComponentes(){
+		this.listSelectAv = (ListView) findViewById(R.id.lv_avaliadores);
+		this.btFinaliza = (Button) findViewById(R.id.bt_finaliza);
+		
+		
+	}
+
+}
