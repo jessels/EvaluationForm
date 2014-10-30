b349f8c9189501ead9bac20e5f53558f7faf62f5
Jessé Luis Silveira <jesse-silveira@hotmail.com>
2014-10-30 16:57:07 -0200

    30/10/2014 - Tabela

--- EvaluationForm/src/br/com/evaluationform/CriarTabela.java
+++ EvaluationForm/src/br/com/evaluationform/CriarTabela.java
@@ -1,6 +1,7 @@
 package br.com.evaluationform;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -13,7 +14,8 @@ import br.com.evaluationform.dao.TabelaAvaliativaDAO;
 public class CriarTabela extends Activity {
 	
 	private EditText nomeTabela;
-	private Button salvar;
+	private Button cria;
+	private Button volta;
 	private TabelaAvaliativaDAO tabelaDAO;
 	private CriterioDAO criterioDAO;
 	
@@ -29,20 +31,29 @@ public class CriarTabela extends Activity {
 		}
 		this.inicializaComponentes();
 		
-		salvar.setOnClickListener(new OnClickListener() {
+		cria.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				String nomeInsere = nomeTabela.getText().toString();
 			}
 		});
-		
+		volta.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent voltaMenu = new Intent(getApplicationContext(), TelaPrincipal.class);
+				startActivity(voltaMenu);
+			}
+		});
 	}
 	
 	private void inicializaComponentes(){
 		this.nomeTabela = (EditText) findViewById(R.id.edNomeTabela);
-		this.salvar = (Button) findViewById(R.id.btSalvar);
+		this.cria = (Button) findViewById(R.id.bt_menu_projeto_criar);
+		this.volta = (Button) findViewById(R.id.bt_menu_projeto_voltar);
 		this.tabelaDAO = new TabelaAvaliativaDAO();
+		
 		
 	}
 
