39c6fa791fe05a61f0c3e8a81b229bff2ecb366f
Jessé Luis Silveira <jesse-silveira@hotmail.com>
2014-11-05 13:38:28 -0200

    05/11/2014 - Criar a avaliação

---
+++ EvaluationForm/src/br/com/evaluationform/CriarAvaliacao.java
@@ -0,0 +1,14 @@
+package br.com.evaluationform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CriarAvaliacao extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
